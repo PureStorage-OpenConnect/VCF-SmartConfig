414eb73ccb99b507bf230b6486b2a4137ac1170f
kylegrossmiller <45182968+kylegrossmiller@users.noreply.github.com>
2020-05-28 15:18:06 -0700

    Add files via upload

---
+++ DNS_DHCP_for_ESXiHosts.ps1
@@ -0,0 +1,18 @@
+﻿#$ScopeName example:  192.168.108.0
+#$DomainName example:  vsphere.local
+#$DataPath example:  C:\VCF_SC.csv
+
+$ScopeName = read-host -Prompt "Type in DHCP Scope Name"  
+$DomainName = read-host -Prompt "Type in DNS Zone Name" 
+$DataPath = read-host -prompt "Type in CSV file path"
+$DhcpScope = Get-DhcpServerv4Scope | where-object{ $_.name.tolower() -eq $ScopeName.ToLower() } 
+$ESXiHosts = import-csv -Path $dataPath -Delimiter "," 
+ 
+foreach( $r in $ESXiHosts ) 
+{ 
+    if ( $r.'MAC' -eq $null ) { continue } 
+    $m = ($r.'MAC').replace( ":", "-") 
+    Add-DnsServerResourceRecordA -ZoneName $DomainName -Name $r.'FQDN' -IPv4Address $r.'IP' -CreatePtr
+    Add-DhcpServerv4Reservation -ScopeId $ScopeName -IPAddress $r.'IP' -Name $r.'FQDN' -ClientId $m -Type Dhcp 
+   
+} 
